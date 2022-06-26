096e2d9ed6cdced8ac083ebfd53255ff284c4dc5
yujinyang1998 <yujinyang1998@users.noreply.github.com>
2022-06-26 13:37:27 +0800

    Fix chat null bug

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,13 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react'
-import styled from 'styled-components'
-import Logout from './Logout'
-import ChatInput from './ChatInput'
-import axios from 'axios'
-import { getAllMessagesRoute, sendMessageRoute } from '../utils/APIRoutes'
-import { v4 as uuidv4 } from "uuid"
+import React, { useState, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+import Logout from './Logout';
+import ChatInput from './ChatInput';
+import axios from 'axios';
+import { getAllMessagesRoute, sendMessageRoute } from '../utils/APIRoutes';
+import { v4 as uuidv4 } from 'uuid';
 
-export default function ChatContainer({ currentChat , currentUser , socket }) {
-
+export default function ChatContainer({ currentChat, currentUser, socket }) {
   const [messages, setMessages] = useState([]);
   const [arrivalMessage, setArrivalMessage] = useState(null);
   const scrollRef = useRef();
@@ -15,51 +14,51 @@ export default function ChatContainer({ currentChat , currentUser , socket }) {
   useEffect(() => {
     async function getCurrent() {
       if (currentChat && currentUser) {
-        const {data} = await axios.post(getAllMessagesRoute, {
+        const { data } = await axios.post(getAllMessagesRoute, {
           from: currentUser._id,
           to: currentChat._id,
-      })
-      setMessages(data);
-      // console.log(data)
+        });
+        setMessages(data);
+        // console.log(data)
+      }
+      // console.log(currentChat, currentUser)
     }
-    // console.log(currentChat, currentUser)
-  }
-  getCurrent();
-  }, [currentChat])
+    getCurrent();
+  }, [currentChat, arrivalMessage, currentUser]);
 
   const handleSendMsg = async (msg) => {
     await axios.post(sendMessageRoute, {
       from: currentUser._id,
       to: currentChat._id,
       message: msg,
-    })
-    socket.current.emit("send-msg", {
+    });
+    socket.current.emit('send-msg', {
       to: currentChat._id,
       from: currentUser._id,
       message: msg,
-    })
+    });
 
     const msgs = [...messages];
-    msgs.push({fromSelf: true, message: msg})
-    setMessages(msgs)
-  }
+    msgs.push({ fromSelf: true, message: msg });
+    setMessages(msgs);
+  };
 
   useEffect(() => {
-    if(socket.current) {
-      socket.current.on("msg-receive", (msg) => {
-        setArrivalMessage({fromSelf:false, message:msg})
-      })
+    if (socket.current) {
+      socket.current.on('msg-receive', (msg) => {
+        setArrivalMessage({ fromSelf: false, message: msg });
+      });
     }
-  },[])
+  }, []);
 
   useEffect(() => {
     // console.log(arrivalMessage)
-    arrivalMessage && setMessages((prev) => [...prev, arrivalMessage])
-  },[arrivalMessage])
+    arrivalMessage && setMessages((prev) => [...prev, arrivalMessage]);
+  }, [arrivalMessage]);
 
   useEffect(() => {
-    scrollRef.current?.scrollIntoView({behaviour:"smooth"})
-  },[messages])
+    scrollRef.current?.scrollIntoView({ behaviour: 'smooth' });
+  }, [messages]);
 
   return (
     <>
@@ -68,10 +67,7 @@ export default function ChatContainer({ currentChat , currentUser , socket }) {
           <div className='chat-header'>
             <div className='user-details'>
               <div className='avatar'>
-                <img
-                  src={`data:image/svg+xml;base64,${currentChat.avatarImage}`}
-                  alt='avatar'
-                />
+                <img src={`data:image/svg+xml;base64,${currentChat.avatarImage}`} alt='avatar' />
               </div>
               <div className='username'>
                 <h3>{currentChat.username}</h3>
@@ -79,28 +75,24 @@ export default function ChatContainer({ currentChat , currentUser , socket }) {
             </div>
             <Logout />
           </div>
-          <div className="chat-messages">
+          <div className='chat-messages'>
             {messages.map((message) => {
-                return (
-                  <div ref = {scrollRef} key = { uuidv4() }>
-                    <div 
-                    className={`message ${
-                      message.fromSelf ? "sended":"received"
-                    }`}>
-                      <div className="content">
-                        <p>{message.message}</p>
-                      </div>
+              return (
+                <div ref={scrollRef} key={uuidv4()}>
+                  <div className={`message ${message.fromSelf ? 'sended' : 'received'}`}>
+                    <div className='content'>
+                      <p>{message.message}</p>
                     </div>
                   </div>
-                )
-              })
-            }
+                </div>
+              );
+            })}
           </div>
-          <ChatInput handleSendMsg = {handleSendMsg}/>
+          <ChatInput handleSendMsg={handleSendMsg} />
         </Container>
       )}
     </>
-  )
+  );
 }
 
 const Container = styled.div`
@@ -133,24 +125,24 @@ const Container = styled.div`
       }
     }
   }
-  .chat-messages{
+  .chat-messages {
     padding: 1rem 2rem;
     display: flex;
     flex-direction: column;
     gap: 1rem;
     overflow: auto;
-    &::-webkit-scrollbar{
+    &::-webkit-scrollbar {
       width: 0.2rem;
-      &-thumb{
+      &-thumb {
         background-color: #ffffff39;
         width: 0.1rem;
         border-radius: 1rem;
       }
     }
-    .message{
+    .message {
       display: flex;
       align-items: center;
-      .content{
+      .content {
         max-width: 40%;
         overflow-wrap: break-word;
         padding: 0.5rem;
@@ -159,17 +151,17 @@ const Container = styled.div`
         color: #d1d1d1;
       }
     }
-    .sended{
+    .sended {
       justify-content: flex-end;
-      .content{
+      .content {
         background-color: #4d04ff21;
       }
     }
-    .received{
+    .received {
       justify-content: flex-start;
-      .content{
+      .content {
         background-color: #9900ff20;
       }
     }
   }
-`
+`;
