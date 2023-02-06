f7e9ed5a4442aa681d5d49ac00baf5dc93bb0076
Mackenzie Yu <e0878502@u.nus.edu>
2023-02-07 00:21:58 +0800

    Update APIRoutes.js

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
 export const host = 'http://localhost:4000'
+// pre-create all routes where corresponding controllers are at, which will be used to handle all types of requests
 export const signupRoute = `${host}/api/auth/signup`
 export const loginRoute = `${host}/api/auth/login`
 export const setAvatarRoute = `${host}/api/auth/setavatar`
